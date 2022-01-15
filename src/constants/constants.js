cab2f53790b7e067db6a24f3f5dbb6414b787366
Shebl <islam.shebl87@gmail.com>
2022-01-15 14:51:57 +0200

    initial

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Netflix Clone',
+    description: "Using React, TMDB API,  Creating Netflix Clone Application - from Scratch The App is called Sheblmoviesdb and hosted in netlify",
+      image: '/images/nfcss.png',
+      tags: ['React', 'TMDB', 'Netflix'],
+    source: 'https://github.com/islamshebl87/netflix-clone',
+    visit: 'https://sheblmoviesdb.netlify.app/',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'WebRTC App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      image: '/images/3.jpg',
+      tags: ['React', 'WebRTC'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: '/images/4.jpg',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey when I was a Computer Teacher ', },
+  { year: 2018, text: 'Started with HTML/CSS/JAVASCRIPT', },
+  { year: 2019, text: 'Founded Artstech Store Facebook Page', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Worked as a freelance developer', },
+];
